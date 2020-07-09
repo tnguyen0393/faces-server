3e020a80b509ebcf28bf4a936e3c17b9f7444609
Tien Nguyen <tnguyen0393@gmail.com>
2020-07-09 13:28:53 -0500

    moved clarifai to backend for api privacy

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '6824cef6c5e5498cbbe34cf39399c5da',
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict('a403429f2ddf4b49b307e318f00e528b', req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json('unable to work with API'));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
@@ -12,4 +27,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage,
+  handleApiCall,
 };
