f11d9fcead130e6f5d1092de42a296cc1f7db3a8
Tien Nguyen <tnguyen0393@gmail.com>
2020-07-09 23:50:49 -0500

    revert

--- server.js
+++ server.js
@@ -22,36 +22,29 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
   res.send('It is working!!!');
 });
 
 app.post('/signin', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
   signin.handleSignin(req, res, db, bcrypt);
 });
 
 app.post('/register', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
   register.handleRegister(req, res, db, bcrypt);
 });
 
 app.get('/profile/:id', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
   profile.handleProfileGet(req, res, db);
 });
 
 app.put('/image', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
   image.handleImage(req, res, db);
 });
 
 app.post('/imageurl', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
   image.handleApiCall(req, res);
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  res.set('Access-Control-Allow-Origin', '*');
   console.log(`App is running on port ${process.env.PORT}`);
 });
